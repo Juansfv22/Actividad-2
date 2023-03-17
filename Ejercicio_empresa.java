6a3b568ebd2275d8c780704d04a5db9fa9d97300
Juansfv22 <126702354+Juansfv22@users.noreply.github.com>
2023-03-17 12:31:01 -0500

    Add files via upload

---
+++ Ejercicio_empresa.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_empresa;
+import java.util.Scanner;
+public class Ejercicio_empresa {
+    public static void main(String[] args) {
+        double VD1,VD2,VD3,salario,porven,salario1,salario2,salario3;
+        Scanner valores = new Scanner(System.in);
+        System.out.println("Ventas del departamento 1:");
+        VD1=valores.nextDouble();
+        System.out.println("Ventas del departamento 2:");
+        VD2=valores.nextDouble();
+        System.out.println("Ventas del departamento 3:");
+        VD3=valores.nextDouble();
+        System.out.println("Salario de los vendedores");
+        salario=valores.nextDouble();
+        porven=(VD1+VD2+VD3)*0.33;
+        if (VD1>porven)
+            salario1=salario*1.2;
+        else
+            salario1=salario;
+        if (VD2>porven)
+            salario2=salario*1.2;
+        else
+            salario2=salario;
+        if (VD3>porven)
+            salario3=salario*1.2;
+        else
+            salario3=salario;
+        System.out.println("Salario vendedores departamento 1: $"+salario1);
+        System.out.println("Salario vendedores departamento 2: $"+salario2);
+        System.out.println("Salario vendedores departamento 3: $"+salario3);
+    }
+}
