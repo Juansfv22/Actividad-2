10a6149dbf538149bf807f728e1d38237703b52d
Juansfv22 <126702354+Juansfv22@users.noreply.github.com>
2023-03-17 01:51:34 -0500

    Add files via upload

---
+++ Ejercicio_universidad.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_universidad;
+import java.util.Scanner;
+public class Ejercicio_universidad {
+    public static void main(String[] args) {
+        String NI,NOM; double PAT,EST,PAGMAT=50000;
+        Scanner datos = new Scanner(System.in);
+        System.out.println("Numero de inscripcion del estudiante:");
+        NI=datos.next();
+        datos.nextLine();
+        System.out.println("Nombre del estudiante:");
+        NOM=datos.nextLine();
+        System.out.println("Patrimonio del estudiante:");
+        PAT=datos.nextDouble();
+        System.out.println("Estrato del estudiante:");
+        EST=datos.nextDouble();
+        if (PAT>2000000 && EST>3)
+            PAGMAT+=PAT*0.03;
+        System.out.println("El estudiante con numero de inscripcion "+NI+" y nombre "+NOM+" debe pagar $"+PAGMAT);
+    }
+}
