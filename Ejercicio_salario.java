32f3c53b4d81beb2e388730a77073bdfe5c8caea
Juansfv22 <126702354+Juansfv22@users.noreply.github.com>
2023-03-17 02:52:47 -0500

    Add files via upload

---
+++ Ejercicio_salario.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_salario;
+import java.util.Scanner;
+public class Ejercicio_salario {
+    public static void main(String[] args) {
+        String nombre; double horas_semana,pago_hora,horas_extra,salario;
+        Scanner datos = new Scanner(System.in);
+        System.out.println("Nombre del trabajador:");
+        nombre=datos.nextLine();
+        System.out.println("Horas trabajadas en la semana:");
+        horas_semana=datos.nextDouble();
+        System.out.println("Dinero recibido por una hora:");
+        pago_hora=datos.nextDouble();
+        horas_extra=horas_semana-40;
+        if (horas_extra>0)
+            if (horas_extra>8)
+                salario=(40*pago_hora)+(8*pago_hora*2)+((horas_semana-48)*pago_hora*3);
+            else
+                salario=(40*pago_hora)+(horas_extra*pago_hora*2);
+        else
+            salario=horas_semana*pago_hora;
+        System.out.println("El trabajador "+nombre+" devengo $"+salario);
+    }
+}
