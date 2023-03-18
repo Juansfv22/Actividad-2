6152f199642f808eae1e15164139b216c07669ca
Juansfv22 <126702354+Juansfv22@users.noreply.github.com>
2023-03-18 08:35:57 -0500

    Add files via upload

---
+++ Ejercicio_empleado22.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ejercicio_empleado22;
+import java.util.Scanner;
+public class Ejercicio_empleado22 {
+    public static void main(String[] args) {
+        String nombre; double salario_hora,horas_mes,salario_mes;
+        Scanner datos = new Scanner(System.in);
+        System.out.println("Nombre del empleado: ");
+        nombre=datos.nextLine();
+        System.out.println("Salario basico por hora: ");
+        salario_hora=datos.nextDouble();
+        System.out.println("Horas trabajadas en el mes: ");
+        horas_mes=datos.nextDouble();
+        salario_mes=salario_hora*horas_mes;
+        if (salario_mes>450000)
+            System.out.println("El salario mensual de "+nombre+" es de $"+salario_mes);
+        else
+            System.out.println("nombre del empleado: "+nombre);
+    }
+    
+}
